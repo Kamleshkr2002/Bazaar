1097e908427919dd70cdb85edb9233c75736a5b5
Kamlesh <kamlesh.kumar23112002@gmail.com>
2025-07-31 22:05:35 +0530

    hi

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <Link to="/" className="text-2xl font-bold">CampusMarket</Link>
+      <div className="space-x-4">
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Navbar from "../components/common/Navbar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="p-6">
+      <Navbar/>
+      <h1 className="text-3xl font-bold mb-4">Explore Items</h1>
+      <p className="text-gray-600">This is the homepage. Product cards will go here.</p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
